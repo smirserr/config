e9fdac8887bc2c5ca7a8f3d7d6c1b021ff166661
smirserr <smirserr@gmail.com>
2013-11-21 23:28:01 +0200

    Теперь архивы создаются без сжатия

--- backup_server/backup.sh
+++ backup_server/backup.sh
@@ -14,8 +14,8 @@ PASSWORD=$5
 TAR=/bin/tar
 # Тип архиватора и расширение. Выбрать либо gzip, либо bzip2
 #gzip
-arch_type="--gzip"
-arch_extension=gz
+arch_type=""
+arch_extension=""
 
 # pwd - текущий рабочий каталог
 SCRIPT_DIR=`pwd`
@@ -24,15 +24,15 @@ SCRIPT_DIR=`pwd`
 DIR_SOURCE="/mnt/backup_dir"
 
 # Лог-файл
-LOG="/var/log/backup.log"
+LOG="/mnt/tcfi_local/_backups/backup.log"
 
 # Где храним бекапы. 
-DIR_TARGET_MONTH="/var/spool/backup/$PCNAME/month"
-DIR_TARGET_DAY="/var/spool/backup/$PCNAME/day"
+DIR_TARGET_MONTH="/mnt/tcfi_local/_backup/$PCNAME/month"
+DIR_TARGET_DAY="/mnt/tcfi_local/_backup/$PCNAME/day"
 
 #Файлы инкримента
-increment="/var/spool/backup/$PCNAME/increment.inc"
-increment_day="/var/spool/backup/$PCNAME/increment_day.inc"
+increment="/mnt/tcfi_local/_backup/$PCNAME/increment.inc"
+increment_day="/mnt/tcfi_local/_backup/$PCNAME/increment_day.inc"
 
 PATH=/usr/local/bin:/usr/bin:/bin
 # текущее число
@@ -43,7 +43,7 @@ echo "$(date)"
 echo "Приступаем $PCNAME"
 echo "Приступаем $PCNAME" >> $LOG
 wakeonlan -p 8 $MAC
-sleep 120
+sleep 200
 echo "Монтируем шару"
 #sudo mount -t cifs  '//'$1'.ds/C$' -o user=$USER,password=$PASSWORD,dom=$DOMAIN,sec=ntlm /mnt/backup_dir -v
 #монтируем шару
@@ -52,18 +52,20 @@ if mount -t cifs  "//$PCNAME.$DOMAIN/D$" -o user=$USER,password=$PASSWORD,sec=nt
 		echo "Делаем полный бэкап"
 		echo "Делаем полный бэкап" >> $LOG
 	# если первое число - делаем полный бэкап, предварительно переименовав предыдущий месячный бэкап, и удалив его инкремент
-		mv $DIR_TARGET_MONTH/full.tar.$arch_extension $DIR_TARGET_MONTH/full.tar.$arch_extension.1
+		mv $DIR_TARGET_MONTH/full.tar $DIR_TARGET_MONTH/full.tar.1
 		rm $increment
-		$TAR --create --ignore-failed-read --one-file-system --recursion --preserve-permissions --sparse --listed-incremental=$increment $arch_type --verbose --file=$DIR_TARGET_MONTH/full.tar.$arch_extension $DIR_SOURCE
+		$TAR --create --ignore-failed-read --one-file-system --recursion --preserve-permissions --sparse --listed-incremental=$increment $arch_type --verbose --file=$DIR_TARGET_MONTH/full.tar $DIR_SOURCE
 		# переименовываем дневные инкрементные бэкапы, старые бекапы удаляем.
-		for i in $( find $DIR_TARGET_DAY/ -name "*tar.$arch_extension.1" ); do rm -f $i; done
-		for i in $( find $DIR_TARGET_DAY/ -name "*tar.$arch_extension" ); do mv $i $i.1; done
+		for i in $( find $DIR_TARGET_DAY/ -name "*tar.1" ); do rm -f $i; done
+		for i in $( find $DIR_TARGET_DAY/ -name "*tar" ); do mv $i $i.1; done
 	else
 		echo "Делаем инкрементный бэкап"
 		echo "Делаем инкрементный бэкап" >> $LOG
 		#если не первое число - делаем инкрементные (только изменения) дневные бекапы
 		cp $increment $increment_day
-		$TAR --create --ignore-failed-read --one-file-system --recursion --preserve-permissions --sparse --listed-incremental=$increment_day $arch_type --verbose --file $DIR_TARGET_DAY/day$DOM.tar.$arch_extension $DIR_SOURCE
+		$TAR --create --ignore-failed-read --one-file-system --recursion --preserve-permissions 
+		--sparse --listed-incremental=$increment_day $arch_type --verbose 
+		--file $DIR_TARGET_DAY/day$DOM.tar $DIR_SOURCE
 	fi
 	umount $DIR_SOURCE
 else 
