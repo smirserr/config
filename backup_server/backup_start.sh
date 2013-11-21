45b827bf87bc3ed6edca66a29e8cafcfff1cee92
smirserr <smirserr@gmail.com>
2013-11-21 14:28:21 +0200

    Скрипт запуска бэкапов fi

---
+++ backup_server/backup_start.sh
@@ -0,0 +1,11 @@
+#pc54 tefimchuk
+sh backup.sh 30:85:A9:97:B9:13 pc54 ds user n9pwib
+
+#pc46 nchervyakova
+sh backup.sh 00:19:D1:98:AB:AE pc46 ds user n9pwib
+
+#pc22 isherbacheva
+#sh backup.sh 50:E5:49:4A:80:80 pc22 ds user n9pwib
+
+#pc32 00:19:D1:98:AE:AD
+#sh backup.sh 00:19:D1:98:AE:AD pc32 ds user n9pwib
