a104770e8cb51e4255f7704d216d181ff30a1c76
smirserr <smirserr@gmail.com>
2013-12-10 15:02:10 +0200

    Добавлены фильтры видеои музыки. Бэкап создается без полного пути Добавлены mac

--- backup_server/backup.sh
+++ backup_server/backup.sh
@@ -18,10 +18,13 @@ arch_type=""
 arch_extension=""
 
 # pwd - tekushchy rabochy katalog
-SCRIPT_DIR=`pwd`
+#SCRIPT_DIR=`pwd`
 
 # Chto bekapim
 DIR_SOURCE="/mnt/backup_dir"
+
+EXCLUDE_FILE="/mnt/tcfi_local/_backups/backup_server/exclude_file"
+
 
 # Log-fayl
 LOG="/mnt/tcfi_local/_backups/backup.log"
@@ -43,10 +46,11 @@ echo "$(date)" >> $LOG
 echo "Pristupayem $PCNAME"
 echo "Pristupayem $PCNAME" >> $LOG
 wakeonlan -p 8 $MAC
-sleep 240
+sleep 330
 echo "Montiruyem sharu"
 #mount -t cifs  "//$PCNAME.$DOMAIN/D$" -o user=$USER,password=$PASSWORD,sec=ntlm $DIR_SOURCE -v; then
 #montiruyem sharu
+cd $DIR_SOURCE
 if smbmount "//$PCNAME.$DOMAIN/D$" $DIR_SOURCE -o username=$USER,password=$PASSWORD,domain=$PCNAME,iocharset=cp1251,codepage=cp866; then	
 	if [ $DOM = "06" ]; then
 		echo "Delayem polny bekap"
@@ -54,7 +58,7 @@ if smbmount "//$PCNAME.$DOMAIN/D$" $DIR_SOURCE -o username=$USER,password=$PASSW
 	# esli pervoye chislo - delayem polny bekap, predvaritelno pereimenovav predydushchy mesyachny bekap, i udaliv ego inkrement
 		mv $DIR_TARGET_MONTH/full.tar $DIR_TARGET_MONTH/full.tar.1
 		rm $increment
-		$TAR --create --ignore-failed-read --one-file-system --recursion --preserve-permissions --sparse --listed-incremental=$increment --verbose --file=$DIR_TARGET_MONTH/full.tar --exclude "Admin/*" --exclude "admin/*" --exclude "RECYCLER/*" --exclude "System Volume Information/*" $DIR_SOURCE
+		$TAR --create --ignore-failed-read --one-file-system --recursion --preserve-permissions --sparse --listed-incremental=$increment --verbose --file=$DIR_TARGET_MONTH/full.tar -X $EXCLUDE_FILE  $DIR_SOURCE
 		# pereimenovyvayem dnevnye inkrementnye bekapy, starye bekapy udalyaem.
 		for i in $( find $DIR_TARGET_DAY/ -name "*tar.1" ); do rm -f $i; done
 		for i in $( find $DIR_TARGET_DAY/ -name "*tar" ); do mv $i $i.1; done
@@ -63,7 +67,7 @@ if smbmount "//$PCNAME.$DOMAIN/D$" $DIR_SOURCE -o username=$USER,password=$PASSW
 		echo "Delayem inkrementny bekap" >> $LOG
 		#esli ne pervoye chislo - delayem inkrementnye (tolko izmeneniya) dnevnye bekapy
 		cp $increment $increment_day
-		$TAR --create --ignore-failed-read --one-file-system --recursion --preserve-permissions --sparse --listed-incremental=$increment_day --verbose --file $DIR_TARGET_DAY/day$DOM.tar --exclude "Admin/*" --exclude "admin/*" --exclude "RECYCLER/*" --exclude "System Volume Information/*" $DIR_SOURCE
+		$TAR --create --ignore-failed-read --one-file-system --recursion --preserve-permissions --sparse --listed-incremental=$increment_day --verbose --file $DIR_TARGET_DAY/day$DOM.tar -X $EXCLUDE_FILE $DIR_SOURCE
 	fi
 	umount $DIR_SOURCE
 else 
@@ -71,12 +75,11 @@ else
 	echo "$(date +%F_%R:%S) Oshibka montirovaniya shary $PCNAME.$DOMAIN" >> $LOG
 fi
 
-sleep 30
+sleep 5
 #Vyklyuchayem komp!
 echo "Vyklyuchayem $PCNAME"
 echo "Vyklyuchayem $PCNAME" >>$LOG 
 echo "$(date)" >> $LOG
 echo "###############################" >> $LOG
 net rpc SHUTDOWN -I $PCNAME.$DOMAIN -f -U $PCNAME/$USER%$PASSWORD
-sleep 30
-net rpc SHUTDOWN -I $PCNAME.$DOMAIN -f -U $PCNAME/$USER%$PASSWORD
+
