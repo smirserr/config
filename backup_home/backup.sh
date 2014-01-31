da9b5c05d515c5747d9a1265db7d9f94b1390356
smirserr <smirserr@gmail.com>
2014-01-31 23:44:41 +0200

    Замена find на ls

--- backup_home/backup.sh
+++ backup_home/backup.sh
@@ -11,6 +11,7 @@ date >> $LOG
 TAR=/bin/tar
 # Что бэкапим 
 DIR_SOURCE="/home/serr"
+DIR_SOURCE_2="/etc"
 #Исключения
 EXCLUDE_FILE="/home/serr/.backup/exclude_file"
 
@@ -22,7 +23,7 @@ DOM=`date +20%y_%m_%d`
 # Задаем переменную, в которой вычисляем размер папки с бекапом
 spacebackup=`du -sm $DIR_TARGET/home/ | awk '{print$1}'`
 # Если папка с бекапом занимает больше max_du Мб
-max_du=7000
+max_du=700
 
 echo ">>>>>>>>>> DEL OLD BACKUP >>>>>>>>>>>>>>>"
 echo ">>>>>>>>>> DEL OLD BACKUP >>>>>>>>>>>>>>>" >> $LOG
@@ -30,13 +31,23 @@ if [ $spacebackup -gt $max_du ];
 then
 	cd $DIR_TARGET/home/
 	# То до тех пор пока размер папки с бэкапами будет больше $max_du Мб
+	FLAG=false
+	n=0
 	while [ `du -sm /$DIR_TARGET/home/ | awk '{print$1}'` -gt "$max_du" ]
 	do
-	# Будем в папке с бэкапами удалять все файлы с расширением "tar", начиная с самых старых
-		find /$DIR_TARGET/home/ -name "*.tar" -and -type f | sort -r | tail -n1 | xargs -i rm '{}' >> $LOG
+		# Будем в папке с бэкапами удалять все файлы, начиная с самых старых
+		#find /$DIR_TARGET/home/ -name "*.tar" -and -type f | sort -r | tail -n1 | xargs -i rm '{}' &>> $LOG
+		ls -t /$DIR_TARGET/home/* | tail -n1 | xargs -i rm '{}' &>> $LOG
+		FLAG=true
+		n=$(($n+1))
 	done
-	echo "Нет подходящих файлов для удаления"
-	echo "Нет подходящих файлов для удаления" >> $LOG
+	if $FLAG; then
+		echo "Удалено $n старых бэкапов..."
+        	echo "Удалено $n старых бэкапов..." >> $LOG
+	else
+		echo "Нет подходящих файлов для удаления"
+		echo "Нет подходящих файлов для удаления" >> $LOG
+	fi
 else
 	echo "Удаление файлов не требуется"
 	echo "Удаление файлов не требуется" >> $LOG
@@ -50,7 +61,7 @@ date +%H:%M:%S >> $LOG
 
 $TAR --create --ignore-failed-read --one-file-system --recursion --preserve-permissions --sparse --verbose --exclude=$DIR_TARGET  --file=$DIR_TARGET/home/home_$DOM.tar -X $EXCLUDE_FILE $DIR_SOURCE 2>>$LOG
 
-$TAR --create --ignore-failed-read --one-file-system --recursion --preserve-permissions --sparse --verbose  --file=$DIR_TARGET/etc/etc_$DOM.tar -X 2>>$LOG
+$TAR --create --ignore-failed-read --one-file-system --recursion --preserve-permissions --sparse --verbose  --file=$DIR_TARGET/home/etc_$DOM.tar $DIR_SOURCE_2 2>>$LOG
 
 echo ">>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>"
 echo ">>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>" >> $LOG
